d855075b2768907111daeed10e620aa984ca2dee
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-07 12:29:57 -0700

    fixed some bugs

--- main.go
+++ main.go
@@ -7,15 +7,11 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/form" {
-		http.Error(w, "404 not found", http.StatusNotFound)
-		return
-	}
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "Parseform() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s\n", name)
