50c3e5735a917d80f37e936a88f6a3b5be80fab8
Leksyking <gbemilekeogundipe@gmail.com>
2022-04-07 10:44:27 -0700

    hello handler

--- main.go
+++ main.go
@@ -6,6 +6,21 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "hello!")
+
+}
 func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fileServer)
@@ -13,7 +28,7 @@ func main() {
 	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Println("Sever started on port 8080")
-	if err := http.ListenAndServe("8080", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 }
